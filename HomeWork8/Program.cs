ef71b976ad08705380323e3150fcaf2e61d09b7a
Bob D <txcavscout@live.com>
2022-05-12 00:09:31 -0700

    Uploaded correct code.

--- HomeWork8/Program.cs
+++ HomeWork8/Program.cs
@@ -1,20 +1,44 @@
-﻿
-using System.Text.RegularExpressions;
+﻿using System.Text.RegularExpressions;
 
 string inserts;
 
 Console.Write("Enter a list of comma separated names: ");
 inserts = Console.ReadLine();
 string newNames = Regex.Replace(inserts, " *, *", ",");
-List<string> names = newNames.Split(',').ToList();
+
+string[] names = newNames.Split(",");
 
 
 
 
-for (int i = 0; i < names.Count; i++)
+
+for (int i = 0; i < names.Length; i++)
 {
     Console.WriteLine($"Hello {names[i]}");
 }
+
+
+//Same code below but built with List<string> instead of arrary
+
+//using System.Text.RegularExpressions;
+
+//string inserts;
+
+//Console.Write("Enter a list of comma separated names: ");
+//inserts = Console.ReadLine();
+//string newNames = Regex.Replace(inserts, " *, *", ",");
+//List<string> names = newNames.Split(',').ToList();
+
+
+
+
+//for (int i = 0; i < names.Count; i++)
+//{
+//    Console.WriteLine($"Hello {names[i]}");
+//}
+
+
+
 
 
 
