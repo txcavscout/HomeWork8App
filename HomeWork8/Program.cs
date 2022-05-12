2e4f632e19d39311d4c2ed21ff76c232f6eb6619
Bob D <txcavscout@live.com>
2022-05-11 23:50:13 -0700

    Add project files.

---
+++ HomeWork8/Program.cs
@@ -0,0 +1,23 @@
+﻿
+using System.Text.RegularExpressions;
+
+string inserts;
+
+Console.Write("Enter a list of comma separated names: ");
+inserts = Console.ReadLine();
+string newNames = Regex.Replace(inserts, " *, *", ",");
+List<string> names = newNames.Split(',').ToList();
+
+
+
+
+for (int i = 0; i < names.Count; i++)
+{
+    Console.WriteLine($"Hello {names[i]}");
+}
+
+
+
+
+
+
